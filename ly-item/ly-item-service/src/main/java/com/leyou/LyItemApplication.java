a41717cf3a08dfb13bbfa8f8db50df069ed1be35
chusijun <15988140248@163.com>
2019-09-09 22:08:23 +0800

    day02

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -3,12 +3,14 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Date:2019/9/8 description:
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.mapper")
 public class LyItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(LyItemApplication.class,args);
